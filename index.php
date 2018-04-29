66c2e2d46115e62b0789ecf5b7fcdf9d5e9248e0
mcdoash <mcdoash@users.noreply.github.com>
2018-04-29 02:42:19 -0400

    sometjing wad changd i think

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
         <link rel="stylesheet" href="main.css">
         <link rel="stylesheet" href="dictionary.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-        <script src='https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/tesseract.js'></script>
 
     </head>
     <body>
+        
         <?php 
             require_once "../private/sql-bits.php";
             $conn = new mysqli($servername, $username, $password, $dbname);
@@ -30,6 +30,13 @@
             }
         ?>
         
+        <?php
+            if($_SERVER["REQUEST_METHOD"] == "POST") {
+                if(!empty($_POST["name"])){
+                    
+                }
+            }
+        ?>
        
         <nav>
             <div id="new-folder">
